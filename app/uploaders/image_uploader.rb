2be286b8fdead7fb763a11a664d5da814d7042b2
irahey <iraolivia.garcia@yahoo.com>
2015-08-17 01:21:42 +0000

    Will have to fix dropbox eventually

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,8 +14,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+
+    if model
+      "uploads/#{model.class.to_s.underscore}/#{model.id}/#{mounted_as}"
+    else
+      "uploads/#{mounted_as}/"
+    end
+
   end
+
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,20 @@
 =begin
 CarrierWave.configure do |config|
   config.dropbox_app_key = ENV["APP_KEY"]
-  config.dropbox_app_secret = ENV["APP_SECRET"]
+  config.dropbox_app_secret = ENV["APP_SECRET"] ["h4u36nlieml4hov"]
   config.dropbox_access_token = ENV["ACCESS_TOKEN"]
-  config.dropbox_access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+  config.dropbox_access_token_secret = ENV["ACCESS_TOKEN_SECRET"]  ["s6nqsrw07flsf64"]
   config.dropbox_user_id = ENV["USER_ID"]
   config.dropbox_access_type = "app_folder"
 end
 =end
 
 CarrierWave.configure do |config|
-  config.dropbox_app_key = ENV["7660xv21w1hjm4d"]
-  config.dropbox_app_secret = ENV["h4u36nlieml4hov"]
-  config.dropbox_access_token = ENV["anbs501uxutktwr9"]
-  config.dropbox_access_token_secret = ENV["s6nqsrw07flsf64"]
-  config.dropbox_user_id = ENV["405175561"]
+  config.dropbox_app_key = "7660xv21w1hjm4d"
+  config.dropbox_app_secret = ENV["APP_SECRET"]
+  config.dropbox_access_token = "anbs501uxutktwr9"
+  config.dropbox_access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+  config.dropbox_user_id = "405175561"
   config.dropbox_access_type = "app_folder"
 end
 
